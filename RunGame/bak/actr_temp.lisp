9160362870e54df2efd80ee470ddb56960fb9e18
Sandesh Gavhane <sandesh.gavhane@gmail.com>
2025-03-20 19:08:26 +0100

    removed unwanted folder (RunGame)

--- RunGame/bak/actr_temp.lisp
+++
@@ -1,5 +0,0 @@
-;;; ACT-R Agent Loader Script 
-(load "C:/DeepSeek_ICA_Agent/actr7.x/load-act-r.lisp") 
-(load "C:/DeepSeek_ICA_Agent/geomates/act-r-experiment.lisp") 
-(run-environment) 
-(load-act-r-model "C:/DeepSeek_ICA_Agent/geomates/DeepSeekAgent.lisp") 
--- RunGame/bak/run-lisp-script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Running Geomates with ACT-R...
-sbcl --load "run-geomates.lisp"
-echo Done!
-pause
